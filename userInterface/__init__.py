3912c2e445503b66210a17bdb8b8fd7205eae491
Tim Ebert <tim.ebert@sap.com>
2021-12-05 20:04:07 +0100

    Read data from mysql in frontend

--- userInterface/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The flask application package.
-"""
-
-from flask import Flask
-app = Flask(__name__)
--- userInterface/app.py
+++ userInterface/app.py
@@ -2,9 +2,21 @@
 Routes and views for the flask application.
 """
 
+import mysql.connector
 from datetime import datetime
+from os import environ
+
+from flask import Flask
 from flask import render_template
-from userInterface import app
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host=environ.get("MYSQL_HOST", "localhost"),
+    user=environ.get("MYSQL_USER", "root"),
+    password=environ.get("MYSQL_PASSWORD", "mysecretpw"),
+    database=environ.get("MYSQL_DATABASE", "prices"),
+)
 
 @app.route('/')
 @app.route('/home')
@@ -16,6 +28,7 @@ def home():
         year=datetime.now().year,
     )
 
+
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
@@ -25,6 +38,7 @@ def contact():
         year=datetime.now().year,
         message='Hier stehen die Kontaktdaten'
     )
+
 
 @app.route('/faq')
 def faq():
@@ -36,18 +50,27 @@ def faq():
         message='Hier findest du Antworten auf wichtige Fragen'
     )
 
+def fetchResult():
+    cur = mydb.cursor(dictionary=True)
+    cur.execute("SELECT * FROM fuel_prices")
+
+    return cur.fetchone()
+
 @app.route('/result')
 def result():
     """Renders the result page."""
-    #Was vom Nutzer eingegeben wird
-    parameters= [
+    # Was vom Nutzer eingegeben wird
+    parameters = [
         {"kraftstoff": "Diesel", "wochentag": "dienstags", "zeitfenster": "8 Uhr - 12 Uhr"}
-        ]
+    ]
 
-    #Was als Ergebnis geliefert wird
-    results= [
-        {"name": "Esso Tankstelle", "street": "SCHOZACHER STR. 51", "plz": "70437", "city": "STUTTGART", "price": "1.299"}
-        ]
+    res = fetchResult()
+
+    # Was als Ergebnis geliefert wird
+    results = [
+        {"name": "Esso Tankstelle", "street": "SCHOZACHER STR. 51", "plz": "70437", "city": "STUTTGART",
+         "price": res["e5"]}
+    ]
 
     return render_template(
         'result.html',
@@ -57,3 +80,12 @@ def result():
         results=results,
         parameters=parameters
     )
+
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
