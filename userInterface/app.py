8c754138b8ee3b84324b61948ad2ffa93d4d993e
nikolacornelia <nikola@braukmueller.de>
2021-12-06 23:34:51 +0100

    Add Chart and Filtering by Post Code
    Error if post code doesn't exist

--- userInterface/app.py
+++ userInterface/app.py
@@ -5,8 +5,11 @@ Routes and views for the flask application.
 import mysql.connector
 from datetime import datetime
 from os import environ
-
+import pandas as pd
+from io import BytesIO
+from matplotlib.figure import Figure
 from flask import Flask, render_template, request
+import base64
 
 app = Flask(__name__)
 
@@ -17,11 +20,10 @@ mydb = mysql.connector.connect(
     database=environ.get("MYSQL_DATABASE", "prices"),
 )
 
-def fetchResult():
-    cur = mydb.cursor(dictionary=True)
-    cur.execute("SELECT * FROM fuel_prices")
-
-    return cur.fetchone()
+def fetchResult(input_post_code):
+    cur = mydb.cursor(dictionary=True, buffered=True)
+    cur.execute('SELECT * FROM fuel_prices WHERE post_code = '+input_post_code)
+    return cur.fetchmany(30)
 
 @app.route('/',methods=["GET", "POST"])
 @app.route('/home',methods=["GET", "POST"])
@@ -31,16 +33,31 @@ def home():
     if request.method == "POST":
         # Was der Benutzer eingegeben hat
         req = request.form
+        print(req)
         parameters = [
             {"kraftstoff": req["kraftstoff"], "wochentag": req["zeit"]}
         ]
 
         # Was als Ergebnis geliefert wird
-        res = fetchResult()
+        print(req["plz"])
+        res = pd.DataFrame(fetchResult(req["plz"]))
         results = [
-            {"name": "Esso Tankstelle", "street": "SCHOZACHER STR. 51", "plz": res["post_code"], "city": "STUTTGART",
+            {"name": "Esso Tankstelle", "street": "SCHOZACHER STR. 51", "plz": req["plz"], "city": "STUTTGART",
             "price": res[req["kraftstoff"]]}
         ]
+
+        # Generate the figure 
+        fig = Figure()
+        ax = fig.subplots()
+        ax.plot(res["window_start"], res["diesel"],label='Diesel')
+        ax.plot(res["window_start"], res["e5"],label='E5')
+        ax.plot(res["window_start"], res["e10"],label='E10')
+        ax.legend(loc="upper left")
+        # Save it to a temporary buffer.
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        plot_url = base64.b64encode(buf.getbuffer()).decode("ascii") 
 
         return render_template(
             'result.html',
@@ -48,7 +65,8 @@ def home():
             year=datetime.now().year,
             message='Das ist die günstigste Tankstelle',
             results=results,
-            parameters=parameters)
+            parameters=parameters,
+            plot_url=plot_url)
 
     return render_template(
         'index.html',
